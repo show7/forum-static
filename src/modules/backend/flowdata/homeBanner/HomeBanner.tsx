7fd8cb1c7597d14e9b9f81bbbba5a621fcee496d
三十文 <1047210087@qq.com>
2018-05-04 17:15:09 +0800

    change showdialog default value

--- src/modules/backend/flowdata/homeBanner/HomeBanner.tsx
+++ src/modules/backend/flowdata/homeBanner/HomeBanner.tsx
@@ -10,7 +10,7 @@ export default class HomeBanner extends React.Component {
     super();
     this.state = {
       banners: [],
-      showDialog: true,
+      showDialog: false,
       imageUrl: '',
       linkUrl: '',
       sequence: 0,
