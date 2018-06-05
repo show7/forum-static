8bedd8004821331a9caba39413ecd3a6bd5bc2a6
richardyangcz <richardyangcz@sina.com>
2018-06-05 15:45:22 +0600

    add channel

---
+++ src/modules/backend/component/RuleConfig.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react'
+import TextField from 'material-ui/TextField'
+import './RuleConfig.less'
+
+/**
+ * 规则配置
+ */
+
+export default class RuleConfig extends React.Component<any, any> {
+
+  constructor(props) {
+    super(props)
+    this.state= {
+      memberTypeIds:'',
+      content:'',
+      link:''
+    }
+  }
+
+  handleOnChange(){
+    const {memberTypeIds,content,link} = this.state;
+    this.props.onChange(memberTypeIds,content,link);
+  }
+
+  handleChangeRule(e,v){
+    this.setState({memberTypeIds:v},()=>{
+      this.handleOnChange();
+    })
+  }
+
+
+  handleChangeContent(e,v){
+    this.setState({content:v},()=>{
+      this.handleOnChange();
+    })
+  }
+
+  handleChangeLink(e,v){
+    this.setState({link:v},()=>{
+      this.handleOnChange();
+    })
+  }
+
+
+  render() {
+    console.log(this.props.data)
+    console.log(this.props.data === 0 )
+    return (
+      <div className="rule-container">
+        {this.props.data !== 0 &&
+          <div>
+            <TextField hintText="请输入规则" multiLine={true} onChange={(e, v) => this.handleChangeRule(e, v)}/>
+          </div>
+        }
+        <div>
+          <TextField hintText="请输入关注文案" multiLine={true} onChange={(e,v)=>this.handleChangeContent(e,v)}/>
+        </div>
+        <div>
+          <TextField hintText="请输入跳转链接" multiLine={true} onChange={(e,v)=>this.handleChangeLink(e,v)}/>
+        </div>
+      </div>
+    )
+  }
+
+}
--- src/modules/backend/operation/GenerateQrCode.tsx
+++ src/modules/backend/operation/GenerateQrCode.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import { startLoad, endLoad, alertMsg } from 'redux/actions'
-import { loadQrCode } from './async'
+import { loadQrCode, loadQrCodeByRule } from './async'
 import { connect } from 'react-redux'
 import { RaisedButton, TextField } from 'material-ui'
 import './GenerateQrCode.less'
 import * as _ from 'lodash'
+import RuleConfig from '../component/RuleConfig'
 
 /**
  * 推广二维码
@@ -17,22 +18,34 @@ export default class GenerateQrCode extends React.Component<any, any> {
     this.state = ({
       scene: '',
       img: '',
-      remark: ''
+      remark: '',
+      ruleList: [],
+      ruleSum: 1
     })
   }
 
   componentWillMount(props) {
+
   }
 
   generateQrCode = () => {
     const { dispatch } = this.props
-    const { scene,remark } = this.state
-    if(_.isEmpty(scene) || _.isEmpty(remark)){
+    const { scene, remark, ruleList } = this.state
+    if(_.isEmpty(scene) || _.isEmpty(remark)) {
       dispatch(alertMsg('请输入活动英文名和中文活动名称'))
       return
     }
+    if(ruleList.length < 1) {
+      dispatch(alertMsg('请输入默认规则'))
+      return
+    }
+    const param = {
+      scene,
+      remark,
+      ruleList
+    }
     dispatch(startLoad())
-    loadQrCode(scene,remark).then(res => {
+    loadQrCodeByRule(param).then(res => {
       dispatch(endLoad())
       const { code, msg } = res
       if(code === 200) {
@@ -47,8 +60,31 @@ export default class GenerateQrCode extends React.Component<any, any> {
     })
   }
 
+  addRule() {
+    this.setState({ ruleSum: this.state.ruleSum + 1 })
+  }
+
+  handleRule(memberTypeIds, link, content, id) {
+    let { ruleList } = this.state
+    ruleList[id] = { memberTypeIds, link, content }
+    this.setState({
+      ruleList: ruleList
+    })
+  }
+
   render() {
     const { scene, img, remark } = this.state
+
+    const renderRules = () => {
+      let rules = []
+      for(let i = 0; i < this.state.ruleSum; i++) {
+        rules.push(
+          <RuleConfig key={i} data={i}
+                      onChange={(memberTypeIds, link, content) => this.handleRule(memberTypeIds, link, content, i)}/>
+        )
+      }
+      return rules
+    }
 
     const renderScene = () => {
       return (
@@ -57,11 +93,18 @@ export default class GenerateQrCode extends React.Component<any, any> {
                      onChange={(e, v) => this.setState({ scene: v })}/>
           <TextField floatingLabelText='请输入中文活动名称' value={remark} onChange={(e, v) => this.setState({ remark: v })}/>
           <div>
-          <RaisedButton
-            label="点击生成" primary={true}
-            style={{ marginLeft: 50 }}
-            onClick={() => this.generateQrCode()}
-          />
+            {renderRules()}
+            <RaisedButton
+              label="增加规则" primary={true}
+              style={{ marginLeft: 50, marginBottom: 50 }}
+              onClick={() => this.addRule()}/>
+          </div>
+          <div>
+            <RaisedButton
+              label="点击生成" primary={true}
+              style={{ marginLeft: 50 }}
+              onClick={() => this.generateQrCode()}
+            />
           </div>
         </div>
       )
@@ -69,8 +112,11 @@ export default class GenerateQrCode extends React.Component<any, any> {
     return (
       <div className="qrcode-container">
         {renderScene()}
-        {img != '' && <div className="hint-container">右击图片进行保存</div>}
+        {img != '' && <div className="hint-container">
+          <div>活动链接地址：</div>
+          右击图片进行保存</div>}
         <img src={img}/>
+
       </div>
     )
   }
