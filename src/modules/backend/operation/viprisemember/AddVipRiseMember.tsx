7b35fc30efef367422798742031d6bec40146a00
dingzj <justin@iquanwai.com>
2018-08-02 19:02:56 +0800

    update

--- src/modules/backend/operation/viprisemember/AddVipRiseMember.tsx
+++ src/modules/backend/operation/viprisemember/AddVipRiseMember.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
-import { TextField, RaisedButton, SelectField, MenuItem } from 'material-ui'
+import { TextField, RaisedButton, SelectField, MenuItem, DatePicker } from 'material-ui'
 import './AddVipRiseMember.less'
 import { openVipRiseMember, loadRiseMember } from '../async'
 import _ from 'lodash'
 import { connect } from 'react-redux'
 import { set, startLoad, endLoad, alertMsg } from 'redux/actions'
+import { formatDate } from 'utils/helpers'
 
 @connect(state => state)
 export default class AddVipRiseMember extends React.Component {
@@ -16,6 +17,8 @@ export default class AddVipRiseMember extends React.Component {
       memo: '',
       memberTypeId: 0,
       memberTypes: [],
+      startDate: formatDate(new Date(), 'yyyy-MM-dd'),
+      vip: true,
     }
   }
 
@@ -26,7 +29,7 @@ export default class AddVipRiseMember extends React.Component {
   }
 
   handleClickOpenRiseMember() {
-    const { riseId, month, memo, memberTypeId } = this.state
+    const { riseId, month, memo, memberTypeId, vip, startDate } = this.state
     const { dispatch } = this.props
     if(_.isEmpty(riseId) || month === 0 || memberTypeId === 0) {
       dispatch(alertMsg('请补充完整数据再提交'))
@@ -38,7 +41,7 @@ export default class AddVipRiseMember extends React.Component {
       return
     }
 
-    openVipRiseMember(riseId, month, memo, memberTypeId).then(res => {
+    openVipRiseMember({riseId, month, memo, memberTypeId, vip, startDate}).then(res => {
       if(res.code === 200) {
         dispatch(alertMsg('更新成功'))
       } else {
@@ -61,6 +64,13 @@ export default class AddVipRiseMember extends React.Component {
           <MenuItem key={3} value={12} primaryText="12 个月"/>
         </SelectField>
         <br/>
+        <SelectField value={this.state.vip}
+                     floatingLabelText="是否vip"
+                     onChange={(e, idx, value) => this.setState({ vip: value })}>
+          <MenuItem key={1} value={true} primaryText="是"/>
+          <MenuItem key={2} value={false} primaryText="否"/>
+        </SelectField>
+        <br/>
         <SelectField
           value={memberTypeId}
           floatingLabelText="选择会员类型"
@@ -78,6 +88,13 @@ export default class AddVipRiseMember extends React.Component {
             })
           }
         </SelectField>
+        <br/>
+        <DatePicker hintText="选择会员开始时间，默认今天"
+                    formatDate={(date) => formatDate(date, 'yyyy-MM-dd')}
+                    mode="landscape"
+                    onChange={(e, v) => this.setState({
+                      startDate: formatDate(v, 'yyyy-MM-dd'),
+                    })}/>
         <br/>
         <TextField hintText="用户身份（10字以内）" onChange={(e, v) => this.setState({ memo: v })}/>
         <br/>
